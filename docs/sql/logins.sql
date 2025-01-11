27bf5e6794ea10050a19935eb927b2486790eb99
miranda <74680322+123-miranda-123@users.noreply.github.com>
2025-01-11 10:35:25 -0600

    commit

--- docs/sql/logins.sql
+++ docs/sql/logins.sql
@@ -1,14 +1,7 @@
-CREATE TABLE tbl {
-    firstName STR
-    lastName STR
-    userName STR
-    pass STR
-    joinCode INT
-}
+
 CREATE TABLE users (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    firstName VARCHAR(255),
-    lastName VARCHAR(255),
+    name VARCHAR(255),
     email VARCHAR(255) UNIQUE,
     password VARCHAR(255),
     role VARCHAR(50) DEFAULT 'member',  -- Added column for role (team member, team captain, admin)
