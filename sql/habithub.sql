7562a1199576fec3950dcee2d3584c898f428bfa
miranda <74680322+123-miranda-123@users.noreply.github.com>
2025-01-12 15:26:20 -0600

    Update habithub.sql

--- sql/habithub.sql
+++ sql/habithub.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Jan 12, 2025 at 09:02 PM
+-- Generation Time: Jan 12, 2025 at 04:11 AM
 -- Server version: 8.0.40
 -- PHP Version: 8.3.14
 
@@ -11,6 +11,7 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+SET NAMES utf8mb4 COLLATE utf8mb4_general_ci;
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -18,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `habithub`
+-- Database: `if0_38083411_habithub`
 --
 
 -- --------------------------------------------------------
@@ -34,7 +35,7 @@ CREATE TABLE `badges` (
   `team_id` int DEFAULT NULL,
   `badge_name` varchar(255) NOT NULL,
   `awarded_date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -50,7 +51,7 @@ CREATE TABLE `habits` (
   `frequency` enum('Daily','Weekly','Monthly') NOT NULL,
   `goal` int NOT NULL,
   `progress` int DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -65,33 +66,7 @@ CREATE TABLE `habit_logs` (
   `team_id` int DEFAULT NULL,
   `progress` int NOT NULL,
   `log_date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `habit_types`
---
-
-CREATE TABLE `habit_types` (
-  `id` int NOT NULL,
-  `habit_name` varchar(50) NOT NULL,
-  `unit` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `habit_types`
---
-
-INSERT INTO `habit_types` (`id`, `habit_name`, `unit`) VALUES
-(1, 'Exercise', 'Minutes'),
-(2, 'Reading', 'Pages'),
-(3, 'Journaling', 'Entries'),
-(4, 'Meditation', 'Minutes'),
-(5, 'Hydration', 'Cups'),
-(6, 'Sleep', 'Hours'),
-(7, 'Project', 'Hours'),
-(8, 'Skill Learning', 'Hours');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -104,7 +79,7 @@ CREATE TABLE `notifications` (
   `user_id` int NOT NULL,
   `message` text NOT NULL,
   `sent_date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -116,7 +91,7 @@ CREATE TABLE `teams` (
   `id` int NOT NULL,
   `name` varchar(255) NOT NULL,
   `captain_id` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `teams`
@@ -135,7 +110,7 @@ CREATE TABLE `team_goals` (
   `id` int NOT NULL,
   `team_id` int NOT NULL,
   `goal` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -148,7 +123,7 @@ CREATE TABLE `team_members` (
   `user_id` int NOT NULL,
   `team_id` int NOT NULL,
   `role` enum('Member','Captain') DEFAULT 'Member'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `team_members`
@@ -169,7 +144,7 @@ CREATE TABLE `users` (
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `role` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
@@ -178,9 +153,7 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`) VALUES
 (1, 'Miranda Wang', 'miranda@gmail.com', '$2y$10$7y4MsnadopPDC233nC5YSeu9y5.sf99LmBprxtOLa6HIisKOrX.Tm', 'admin'),
 (2, 'Team Captain', 'captain@gmail.com', '$2y$10$UG2nzt0UGiEhaKTbOY6JGOq/JdLFdrjSeVe6157aPcWS3W5PouPUO', 'captain'),
-(3, 'Real Member', 'member@gmail.com', '$2y$10$44ZnixnHA5y.D0Pq8/nYE.Jz8f7HfjpRPvvA9qjAiY99b9/djvQyy', 'member'),
-(4, 'member2', 'member2@gmail.com', '$2y$10$cNguV76ltoNNmVCd4Xte0OjVwHfSZ0v4VaHGO8vtJssUvJutmKi5i', 'member'),
-(5, 'member3', 'member3@gmail.com', '$2y$10$xjq58ma/IYbTVHZQetrIx.I7fH0xbjP8ipGeXjzSVb9t22c0PpoQ6', 'member');
+(3, 'Real Member', 'member@gmail.com', '$2y$10$44ZnixnHA5y.D0Pq8/nYE.Jz8f7HfjpRPvvA9qjAiY99b9/djvQyy', 'member');
 
 --
 -- Indexes for dumped tables
@@ -211,12 +184,6 @@ ALTER TABLE `habit_logs`
   ADD KEY `habit_id` (`habit_id`),
   ADD KEY `user_id` (`user_id`),
   ADD KEY `team_id` (`team_id`);
-
---
--- Indexes for table `habit_types`
---
-ALTER TABLE `habit_types`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `notifications`
@@ -276,12 +243,6 @@ ALTER TABLE `habit_logs`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `habit_types`
---
-ALTER TABLE `habit_types`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
 -- AUTO_INCREMENT for table `notifications`
 --
 ALTER TABLE `notifications`
@@ -309,7 +270,7 @@ ALTER TABLE `team_members`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
