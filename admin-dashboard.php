b3adec195f9a17740f739554d1b623496054da5d
Nicoooojjj <111445679+VisorN-ick@users.noreply.github.com>
2025-01-13 17:55:57 -0600

    strtolower

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -2,7 +2,7 @@
 session_start();
 require_once "database.php";
 
-if ($_SESSION["user_role"] !== "admin") {
+if (strtolower($_SESSION["user_role"]) !== "admin") {
     header("Location: index.php");
 }
 // Ensure user is logged in
